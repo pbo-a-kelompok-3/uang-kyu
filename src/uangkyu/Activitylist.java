ce488a760770e44ffb7879a9244cd38a51204a5c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-24 01:07:11 +0700

    add: menambahkan id untuk aktifitas

--- src/uangkyu/Activitylist.java
+++ src/uangkyu/Activitylist.java
@@ -6,6 +6,7 @@ public class Activitylist {
   public ArrayList<Activity> activities = new ArrayList<Activity>();
   
   public Activitylist insert(Activity activity) {
+    activity.id = this.activities.size();
     this.activities.add(activity);
     System.out.println("Aktifitas " + activity.description + " berhasil ditambahkan");
     return this;
@@ -13,7 +14,7 @@ public class Activitylist {
 
   public Activitylist getAll() {
     for (Activity activity : activities) {
-      System.out.println(activity.description + "\t" + activity.nominal + "\t" + activity.type);
+      System.out.println(activity.id + "\t" + activity.description + "\t" + activity.nominal + "\t" + activity.type);
     }
     return this;
   }
