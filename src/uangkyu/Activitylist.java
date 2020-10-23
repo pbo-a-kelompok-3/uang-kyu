ccee65ceb05be658be0dc11894a7414f0f574fbd
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-23 21:42:11 +0700

    add: membuat method getAll()

--- src/uangkyu/Activitylist.java
+++ src/uangkyu/Activitylist.java
@@ -3,10 +3,17 @@ package uangkyu;
 import java.util.ArrayList;
 
 public class Activitylist {
-  ArrayList<Activity> activities;
+  public ArrayList<Activity> activities = new ArrayList<Activity>();
   
   public Activitylist insert(Activity activity) {
     this.activities.add(activity);
+    return this;
+  }
+
+  public Activitylist getAll() {
+    for (Activity activity : activities) {
+      System.out.println(activity.name + "\t" + activity.nominal + "\t" + activity.type);
+    }
     return this;
   }
 }
