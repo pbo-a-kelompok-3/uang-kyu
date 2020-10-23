1a4cf3f4e0d0225a42f7b3935baa2768b10948bf
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-23 21:41:52 +0700

    update: update property

--- src/uangkyu/Activity.java
+++ src/uangkyu/Activity.java
@@ -1,11 +1,10 @@
 package uangkyu;
 
 public class Activity {
-
-    public static void main(String[] args) {
-
-        int Id,createdAt,updatedAt;
-        String name;
-        String Type;
-    }
+  public int id = 0;
+  public int createdAt = 0;
+  public int updatedAt = 0;
+  public int nominal;
+  public String name;
+  public String type;
 }
