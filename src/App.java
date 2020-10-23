ea2dc1deb0d6e62e25ddc9b0b8082511e51fe606
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-23 20:39:10 +0700

    membuat output pilihan

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    
+    Scanner input = new Scanner(System.in);
+    Integer pilihan = 0;
+
+    while (pilihan != 6) {
+      System.out.println("UANG KYU");
+      System.out.println("1. Masukan aktifitas");
+      System.out.println("2. List aktifitas");
+      System.out.println("3. Cari aktifitas");
+      System.out.println("4. Hapus aktifitas");
+      System.out.println("5. Update aktifitas");
+      System.out.println("6. Keluar program");
+      System.out.println("=====================");
+      System.out.print("Masukkan pilihan anda : ");
+      pilihan = input.nextInt();
+    }
+
+    input.close();
   }
 }
