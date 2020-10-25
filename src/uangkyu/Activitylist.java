dcae582bb99f985c0c4a724f8ca6cf14efd05c38
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-25 21:20:58 +0700

    update: update typing

--- src/uangkyu/Activitylist.java
+++ src/uangkyu/Activitylist.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class Activitylist {
   public ArrayList<Activity> activities = new ArrayList<Activity>();
-  
+
   public Activitylist insert(Activity activity) {
     int activitiesLength = this.activities.size();
-    
+
     if (activitiesLength != 0) {
       Activity lastActivity = this.activities.get(this.activities.size() - 1);
       activity.id = lastActivity.id + 1;
@@ -39,22 +39,27 @@ public class Activitylist {
 
   public Activitylist getAll() {
     for (Activity activity : activities) {
-      System.out.println(activity.id + "\t" + activity.description + "\t" + activity.nominal + "\t" + activity.getTypeAsText());
+      System.out.println(
+          activity.id + "\t" + activity.description + "\t" + activity.nominal + "\t" + activity.getTypeAsText());
     }
     return this;
   }
 
   public Activitylist delete(int id) {
     int index = this.getIndexFromId(id);
-    if(index < 0) System.out.println("Id : " + id + " tidak ditemukan");
-    else this.activities.remove(index);
+    if (index < 0)
+      System.out.println("Id : " + id + " tidak ditemukan");
+    else
+      this.activities.remove(index);
     return this;
   }
 
   public Activitylist update(int id, Activity activity) {
     int index = this.getIndexFromId(id);
-    if(index < 0 ) System.out.println("Id : " + id + " tidak ditemukan");
-    else this.activities.set(index, activity);
+    if (index < 0)
+      System.out.println("Id : " + id + " tidak ditemukan");
+    else
+      this.activities.set(index, activity);
     return this;
   }
 }
