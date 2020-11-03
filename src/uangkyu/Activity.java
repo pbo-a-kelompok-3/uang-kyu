e7ed1d8551c6015df9e93d166f10f2351c7aef78
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-03 14:38:56 +0700

    update: mengubah modifier public menjadi private

--- src/uangkyu/Activity.java
+++ src/uangkyu/Activity.java
@@ -1,12 +1,12 @@
 package uangkyu;
 
 public class Activity {
-  public int id = 0;
-  public int createdAt = 0;
-  public int updatedAt = 0;
-  public int nominal;
-  public String description;
-  public int type;
+  private int id = 0;
+  private int createdAt = 0;
+  private int updatedAt = 0;
+  private int nominal;
+  private String description;
+  private int type;
 
   public String getTypeAsText() {
     return type == 1 ? "Pemasukan" : "Pengeluaran";
