41f6fe0456af295a20ffbb14b09134a01a85f569
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-12-27 14:55:42 +0700

    feat(model-activity): membuat propery beserta setter dan getternya

--- src/Models/Activity.java
+++ src/Models/Activity.java
@@ -10,5 +10,58 @@ package Models;
  * @author Alfian Andi Nugraha
  */
 public class Activity {
-    
+    private int id;
+    private String description;
+    private float nominal;
+    private int typeId;
+    private int createdAt;
+    private int updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getNominal() {
+        return nominal;
+    }
+
+    public void setNominal(float nominal) {
+        this.nominal = nominal;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    public int getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(int createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(int updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
