60663cb2b56c28119166e6ee757e61f75bb027db
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-12-29 21:50:14 +0700

    feat(build): membuat file untuk deploy

--- src/App.java
+++ src/App.java
@@ -1,111 +1,44 @@
-import java.io.Console;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import views.Dashboard;
+/**
+ *
+ * @author Alfian Andi Nugraha
+ */
+public class App {
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Dashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Dashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Dashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Dashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
 
-import uangkyu.Activity;
-import uangkyu.ActivityList;
-
-public class App extends ActivityList {
-  public static void main(String[] args) {
-    App activitylist = new App();
-    Console input = System.console();
-    int pilihan = 0;
-
-    while (pilihan != 6) {
-      System.out.println("UANG KYU");
-      System.out.println("1. Masukan aktifitas");
-      System.out.println("2. List aktifitas");
-      System.out.println("3. Cari aktifitas");
-      System.out.println("4. Hapus aktifitas");
-      System.out.println("5. Update aktifitas");
-      System.out.println("6. Keluar program");
-      System.out.println("=====================");
-      System.out.print("Masukkan pilihan anda : ");
-      try {
-        pilihan = Integer.parseInt(input.readLine());
-      } catch (Exception err) {
-        pilihan = 0;
-        App.errorHandling(err, "Input yang anda masukan bukan berupa angka");
-      }
-
-      App.getEqualLine();
-      switch (pilihan) {
-        case 1:
-
-          System.out.println("TAMBAH AKTIFITAS");
-          System.out.println();
-          Activity activity = new Activity();
-
-          try{
-            System.out.print("Deskripsi\t: ");
-            String description = input.readLine();
-  
-            System.out.print("Nominal\t\t: Rp");
-            int nominal = Integer.parseInt(input.readLine());
-  
-            System.out.print("Type\n(1: Pemasukan/2: Pengeluaran)\t: ");
-            int type = Integer.parseInt(input.readLine());
-            activity.setDescription(description).setNominal(nominal).setType(type);
-            activitylist.insert(activity);
-          } catch (Exception err) {
-            App.errorHandling(err, "Input yang anda masukan bukan berupa angka");
-          }
-
-          break;
-        case 2:
-          System.out.println("LIST AKTIFITAS");
-          activitylist.getAll();
-          break;
-        case 3:
-          System.out.println("CARI AKTIFITAS");
-          System.out.print("Masukkan id untuk dicari: ");
-          int idActivityToShow = Integer.parseInt(input.readLine());
-          activitylist.get(idActivityToShow);
-          break;
-        case 4:
-          System.out.println("HAPUS AKTIFITAS");
-          System.out.print("Masukkan id untuk dihapus : ");
-          int idActivity = Integer.parseInt(input.readLine());
-          activitylist.delete(idActivity);
-          break;
-        case 5:
-          System.out.println("UPDATE AKTIFITAS");
-          System.out.println();
-          System.out.print("Masukkan id yang ingin di update : ");
-          int targetIdToUpdate = Integer.parseInt(input.readLine());
-
-          Activity activityToUpdate = new Activity();
-
-          System.out.print("Deskripsi\t: ");
-          String descriptionToUpdate = input.readLine();
-
-          System.out.print("Nominal\t\t: Rp");
-          int nominalToUpdate = Integer.parseInt(input.readLine());
-
-          System.out.print("Type\n(1: Pemasukan/2: Pengeluaran)\t: ");
-          int typeToUpdate = Integer.parseInt(input.readLine());
-          
-          System.out.println();
-          activityToUpdate
-            .setDescription(descriptionToUpdate)
-            .setNominal(nominalToUpdate)
-            .setType(typeToUpdate);
-          activitylist.update(targetIdToUpdate, activityToUpdate);
-          break;
-        case 6:
-          System.out.println("Keluar dari program...");
-          break;
-      }
-      App.getEqualLine();
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Dashboard().setVisible(true);
+            }
+        });
     }
-  }
-
-  public static void getEqualLine() {
-    System.out.println("====================================");
-  }
-
-  public static void errorHandling(Exception err, String message) {
-    App.getEqualLine();
-    System.out.println("Oppss sesuatu tidak beres...");
-    System.out.println("MESSAGE => " + message);
-    System.out.println("ERROR => " + err);
-  }
 }
