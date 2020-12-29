8e69508765d429aefd30ebd05023f4e3cc3490ac
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-12-29 13:10:14 +0700

    feat(services): mengambahkan ActivityService untuk mengatur crud ke database

--- src/Services/ActivityService.java
+++ src/Services/ActivityService.java
@@ -3,70 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Models;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
+package Services;
+
+import Models.Activity;
 import Utils.Database;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  *
  * @author Alfian Andi Nugraha
  */
-public class Activity {
-    private int id;
-    private String description;
-    private float nominal;
-    private String createdAt;
-    private String updatedAt;
-    protected String tableName = "activities";
-    
-    public int getId() {
-        return id;
-    }
-
-    public Activity setId(int id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Activity setDescription(String description) {
-        this.description = description;
-        return this;
-    }
-
-    public float getNominal() {
-        return nominal;
-    }
-
-    public Activity setNominal(float nominal) {
-        this.nominal = nominal;
-        return this;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public Activity setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-        return this;
-    }
-
-    public String getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public Activity setUpdatedAt(String updatedAt) {
-        this.updatedAt = updatedAt;
-        return this;
-    }
-    
+public class ActivityService extends Activity {
     public void insert() throws Exception {
         String query = String.format(
             "INSERT INTO %s (description, nominal, created_at, updated_at) VALUES (\"%s\", %f, \"%s\", \"%s\")", 
